6898b77d937093f780c4573a3615ca4816b7be6d
Meggetto Francesco <fra_megg@hotmail.com>
2017-03-17 12:59:53 +0000

    Async fetch of Ads from Web API

--- Help2HelpMobileApp/Help2Help/src/pages/home/home.ts
+++ Help2HelpMobileApp/Help2Help/src/pages/home/home.ts
@@ -1,12 +1,23 @@
 ﻿import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AdService } from '../../providers/ad-service/ad-service';
 
 @Component({
     selector: 'page-home',
-    templateUrl: 'home.html'
+    templateUrl: 'home.html',
+    providers: [AdService]
 })
 export class HomePage {
-    constructor(public navCtrl: NavController) {
+    ads: any;
 
+    constructor(public navCtrl: NavController, public adsService: AdService) {
+        this.loadAds();
+    }
+
+    loadAds() {
+        this.adsService.load()
+            .then(data => {
+                this.ads = data;
+            });
     }
 }
---
+++ Help2HelpMobileApp/Help2Help/src/providers/ad-service/ad-service.ts
@@ -0,0 +1,29 @@
+﻿import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AdService {
+    data1: any;
+    constructor(public http: Http) {
+        console.log('Hello PeopleSearch Provider');
+    }
+
+    load() {
+        if (this.data1) {
+            return Promise.resolve(this.data1);
+        }
+
+        // don't have the data yet
+        return new Promise(resolve => {
+            this.http.get('http://help2helpservice.azurewebsites.net/tables/advertisements?ZUMO-API-VERSION=2.0.0')
+                .map(res => res.json())
+                .subscribe(data => {
+                    // we've got back the raw data, now generate the core schedule data
+                    // and save the data for later reference
+                    this.data1 = data;
+                    resolve(this.data1);
+                });
+        });
+    }
+}
