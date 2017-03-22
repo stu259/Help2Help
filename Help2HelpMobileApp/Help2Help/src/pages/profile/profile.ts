e559533f5d4eb87892dd89495b682501cbea0569
WJohnM18 <williamacdonald18@googlemail.com>
2017-03-22 14:46:05 +0000

    Add new profile page. Stuart's code, Francesco forgot to push ;D

--- Help2HelpMobileApp/Help2Help/src/pages/profile/profile.ts
+++ Help2HelpMobileApp/Help2Help/src/pages/profile/profile.ts
@@ -1,12 +1,25 @@
 ﻿import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { User } from '@ionic/cloud-angular';
+import { AdService } from '../../providers/ad-service/ad-service';
 
 @Component({
     selector: 'page-profile',
-    templateUrl: 'profile.html'
+    templateUrl: 'profile.html',
+    providers: [AdService]
 })
 export class ProfilePage {
-    constructor(public navCtrl: NavController) {
 
+    userDetails: any;
+
+    constructor(public navCtrl: NavController, public user: User, public adsService: AdService) {
+        this.loadUserDetails();
+    }
+
+    loadUserDetails() {
+        this.adsService.load()
+            .then(data => {
+                this.userDetails = data;
+            });
     }
 }
