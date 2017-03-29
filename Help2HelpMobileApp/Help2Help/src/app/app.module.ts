0e33dce2a8aa28fc0411daee8922eafde143c719
Sotiris Loizou CS2014 <sotiris.loizou.2014@uni.strath.ac.uk>
2017-03-29 00:11:35 +0000

    Created sms run on phone default text messenger by pressing titlte of ad

--- Help2HelpMobileApp/Help2Help/src/app/app.module.ts
+++ Help2HelpMobileApp/Help2Help/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { ProfilePage } from '../pages/profile/profile';
 import { MyAdsPage } from '../pages/myads/myads';
 import { InsertAdPage } from '../pages/insert-ad/insert-ad';
 import { ViewAdPage } from '../pages/view-ad/view-ad';
-
+import { SMS } from '@ionic-native/sms';
 const cloudSettings: CloudSettings = {
     'core': {
         'app_id': '013473e5'
@@ -43,6 +43,6 @@ const cloudSettings: CloudSettings = {
         PopoverPage,
         ViewAdPage
     ],
-    providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler}, Geolocation]
+    providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler}, Geolocation,SMS]
 })
 export class AppModule { }
--- Help2HelpMobileApp/Help2Help/src/pages/view-ad/view-ad.ts
+++ Help2HelpMobileApp/Help2Help/src/pages/view-ad/view-ad.ts
@@ -2,6 +2,7 @@
 import { NavParams, NavController, ViewController, AlertController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { AdService } from '../../providers/ad-service/ad-service';
+import { SMS } from '@ionic-native/sms';
 
 @Component({
     selector: 'page-view-ad',
@@ -14,6 +15,22 @@ export class ViewAdPage {
     location: string = this.navParams.get('location');
     dateText: string = this.navParams.get('date'); // set default value to be today
 
-    constructor(public navCtrl: NavController, public navParams: NavParams,public viewCtrl: ViewController, private geoLocation: Geolocation, public adsService: AdService, public alertCtrl: AlertController) {
+    constructor(public navCtrl: NavController, private smsVar:SMS,public navParams: NavParams,public viewCtrl: ViewController, private geoLocation: Geolocation, public adsService: AdService, public alertCtrl: AlertController) {
     }
+    createSMS() {
+        var options = {
+            replaceLineBreaks: false,
+            android: {
+                intent: 'INTENT'
+            }
+        }
+        this.smsVar.send("07467718448", "This is my n", options)
+            .then(() => {
+                alert("success");
+            }, () => {
+                alert("failure");
+            });
+            
+    }
+    
 }
